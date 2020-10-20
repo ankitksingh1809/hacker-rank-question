193366740456879552179839f770abc788f4ca84
ankitksingh1809 <72537746+ankitksingh1809@users.noreply.github.com>
2020-10-20 09:49:28 +0530

    Create running_sum.c

---
+++ running_sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int rs(int n){
+    int rs1;
+    if(n = 1){
+        rs1 = 1;
+    }else{
+        rs1 = rs(n - 1) + n;
+    }
+    return rs1;
+}
+
+int main() {
+    printf("%d", rs(25));
+    return 0;
+}
